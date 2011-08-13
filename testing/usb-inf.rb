04a11f053690ab91d531006188005d924fc207dc
Ali Lown <ali@lown.me.uk>
2011-08-13 21:56:33 +0100

    sw> Add a sort of gui for 'sp' that renders a svg and then shows it

--- testing/usb-inf.rb
+++ testing/usb-inf.rb
@@ -1,8 +1,17 @@
 #!/usr/bin/ruby
 
+require 'rubygems'
+require 'scruffy'
+require 'Qt4'
+
 require 'readline'
+
 require 'lib/scope'
 include USBScope
+
+def renorm(x)
+  x.to_f/255.0*3.3
+end
 
 scope = Scope.new
 
@@ -39,6 +48,33 @@ until (action = Readline.readline("?>",true)) == "q"
         puts "ran out of data"
       end
     end
-  end
+  when "sp"
+    t = Time.now
+    d = scope.scoperead
 
+    da = []
+    db = []
+    which = 0;
+    d.each_byte do
+      |x|
+      da.push renorm x if which == 0
+      db.push renorm x if which == 1
+      which = (which == 0) ? 1 : 0
+    end
+
+    g = Scruffy::Graph.new
+    g.title = "Scope read at #{t}"
+    g.renderer = Scruffy::Renderers::Standard.new
+    g.add :line, 'A', da
+    g.add :line, 'B', db
+    path = "/tmp/tmp/s-#{t.to_i}.svg"
+    g.render :to => path
+
+    app = Qt::Application.new(ARGV)
+    sk = Qt::SvgWidget.new()
+    sk.load(path)
+    sk.show
+    app.exec
+
+  end
 end
