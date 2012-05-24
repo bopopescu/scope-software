180b2a727ab3eda37aa1852ab846ef40224d4a7f
Ali Lown <ali@lown.me.uk>
2012-05-24 20:31:26 +0100

    Add sawtooth, triangle, square gen modes to the demo

--- demo/genWave.rb
+++ demo/genWave.rb
@@ -1,13 +1,32 @@
+#Setup
 if ARGV.length < 3
-  $stderr.puts "Wrong number of arguments\nUsage: genWave.rb numSamples samplesPerCycle offset"
+  $stderr.puts "Wrong number of arguments\nUsage: genWave.rb numSamples samplesPerCycle offset wave"
   exit
 end
 
 numSamples = ARGV[0].to_i
 samplesPerCycle = ARGV[1].to_i
 offset = ARGV[2].to_i
+wave = ARGV[3].downcase unless ARGV[3].nil?
 
+#Make some data
+data = []
 numSamples.times do
   |i|
-  puts Math.sin(Math::PI*i/samplesPerCycle + offset)
+  t = Math::PI*i/samplesPerCycle
+  if wave == "sawtooth" then
+    data.push (2*(t-t.floor + offset) - 1)
+  elsif wave == "triangle" then
+    data.push ((t-2*((t+1)/2).floor)*(-1)**(((t+1)/2).floor))
+  else
+    data.push Math.sin(t + offset)
+  end
 end
+
+#Post process into the correct waveform if needed
+if wave == "square" then
+  data.map!{|x| (x>=0) ? 1 : 0;}
+end
+
+#Output
+data.each{|x| puts x}
--- demo/plotWave.sh
+++ demo/plotWave.sh
@@ -1,9 +1,9 @@
 #!/bin/zsh
 gen()
 {
-  ruby genWave.rb $1 $2 $3 > /tmp/plot.data
+  ruby genWave.rb $1 $2 $3 $4 > /tmp/plot.data
   gnuplot -e "set terminal png size 1920,1080;\
-              set output \"/tmp/plot.$1.$2.$3.png\";\
+              set output \"/tmp/plot.$4.$3.$1.$2.png\";\
               set yrange [-1:1];\
               plot \"/tmp/plot.data\",
               \"/tmp/plot.data\" with lines title \"lines\",\
@@ -11,6 +11,7 @@ gen()
               \"/tmp/plot.data\" smooth bezier with lines title \"bezier\";"
 }
 
+#Sine
 gen 250 50 0
 gen 250 50 5
 gen 250 50 9
@@ -31,4 +32,68 @@ gen 10 2 0
 gen 10 2 5
 gen 10 2 9
 
+#Triangle
+gen 250 50 0 triangle
+gen 250 50 5 triangle
+gen 250 50 9 triangle
+
+gen 100 20 0 triangle
+gen 100 20 5 triangle
+gen 100 20 9 triangle
+
+gen 50 10 0 triangle
+gen 50 10 5 triangle
+gen 50 10 9 triangle
+
+gen 10 5 0 triangle
+gen 10 5 5 triangle
+gen 10 5 9 triangle
+
+gen 10 2 0 triangle
+gen 10 2 5 triangle
+gen 10 2 9 triangle
+
+#Square
+gen 250 50 0 square
+gen 250 50 5 square
+gen 250 50 9 square
+
+gen 100 20 0 square
+gen 100 20 5 square
+gen 100 20 9 square
+
+gen 50 10 0 square
+gen 50 10 5 square
+gen 50 10 9 square
+
+gen 10 5 0 square
+gen 10 5 5 square
+gen 10 5 9 square
+
+gen 10 2 0 square
+gen 10 2 5 square
+gen 10 2 9 square
+
+#Sawtooth
+gen 250 50 0 sawtooth
+gen 250 50 5 sawtooth
+gen 250 50 9 sawtooth
+
+gen 100 20 0 sawtooth
+gen 100 20 5 sawtooth
+gen 100 20 9 sawtooth
+
+gen 50 10 0 sawtooth
+gen 50 10 5 sawtooth
+gen 50 10 9 sawtooth
+
+gen 10 5 0 sawtooth
+gen 10 5 5 sawtooth
+gen 10 5 9 sawtooth
+
+gen 10 2 0 sawtooth
+gen 10 2 5 sawtooth
+gen 10 2 9 sawtooth
+
+#View
 feh /tmp/plot.*.png
