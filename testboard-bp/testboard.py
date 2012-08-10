31d9d7b78c0c61c569c96031e73af9699dda6bf4
Ali Lown <ali@lown.me.uk>
2012-08-10 22:43:35 +0100

    Analog test board: Implement DAC debugging, auxilary power control

--- testboard-bp/testboard.py
+++ testboard-bp/testboard.py
@@ -9,73 +9,76 @@ import sys
 
 def setDACValue(device, addr, value):
   #Assemble the 2 data bytes
-  datahigh = (value >> 6) | 0
-  datalow = (value & 0xFF) << 2
+  datahigh = (value >> 6) & 0xFF
+  datalow = (value & 0xFF)
 
   #Send
   bp = I2Chigh(device, 115200, 1)
   bp.BBmode()
   bp.enter_I2C()
-  bp.cfg_pins(I2CPins.POWER | I2CPins.PULLUPS)
+  bp.cfg_pins(I2CPins.PULLUPS)
 
   bp.send_start_bit()
   stat = bp.bulk_trans(1, [addr<<1 | 0])
+  print "stat: %u %x %u" % (stat[0],stat[1],stat[2])
   if stat[0] == chr(0x01):
     raise IOError, "DAC didn't ack address and write"
   stat = bp.bulk_trans(1, [datahigh])
+  print "stat: %u %x %u" % (stat[0],stat[1],stat[2])
   if stat[0] == chr(0x01):
     raise IOError, "DAC didn't ack top bits"
   stat = bp.bulk_trans(1, [datalow])
+  print "stat: %u %x %u" % (stat[0],stat[1],stat[2])
   if stat[0] == chr(0x01):
     raise IOError, "DAC didn't ack bottom bits"
   bp.send_stop_bit()
   bp.resetBP()
 
 
-def setLMHOptions(device, lp, highgain, ladder):
+def setLMHOptions(device, lp, highgain, ladder, disaux):
   #Assemble the 2 data bytes
-  datahigh = 0 | ((lp & 0x04) >> 2)
+  datahigh = 0 | ((disaux & 0x01) << 2) | ((lp & 0x04) >> 2)
   datalow = ((lp & 0x03) << 6) | ((highgain & 0x01) << 4) | (ladder & 0x0F)
-
+  bp = RAW_WIRE(device, 115200)
   print "Current LMH options:"
-  readLMHOptions(device)
+  readLMHOptions(bp)
   print "-------------------"
 
   #Send
-  bp = RAW_WIRE(device, 115200)
-  bp.BBmode()
-  bp.enter_rawwire()
-  bp.raw_cfg_pins(PinCfg.POWER | PinCfg.CS)
-  if not bp.cfg_raw_wire((RAW_WIRECfg.BIT_ORDER & RAW_WIRE_BIT_ORDER_TYPE.MSB) |
-                         (RAW_WIRECfg.WIRES & RAW_WIRE_WIRES_TYPE.TWO) |
-                         (RAW_WIRECfg.OUT_TYPE & RAW_WIRE_OUT_TYPE._3V3)):
-    raise IOError, "Failed to set pin type correctly"
-  bp.set_speed(RAW_WIRESpeed._400KHZ)
-  bp.CS_Low()
-  bp.bulk_trans(3, [0x00, datahigh, datalow])
-  bp.CS_High()
-  bp.resetBP()
+  #bp.BBmode()
+  #bp.enter_rawwire()
+  #bp.raw_cfg_pins(PinCfg.CS)
+  #if not bp.cfg_raw_wire((RAW_WIRECfg.BIT_ORDER & RAW_WIRE_BIT_ORDER_TYPE.MSB) |
+  #                       (RAW_WIRECfg.WIRES & RAW_WIRE_WIRES_TYPE.TWO) |
+  #                       (RAW_WIRECfg.OUT_TYPE & RAW_WIRE_OUT_TYPE._3V3)):
+  #  raise IOError, "Failed to set pin type correctly"
+  #bp.set_speed(RAW_WIRESpeed._400KHZ)
+  #bp.CS_Low()
+  #bp.bulk_trans(3, [0x00, datahigh, datalow])
+  #bp.CS_High()
+  #bp.resetBP()
 
   print "New LMH options:"
-  readLMHOptions(device)
+  readLMHOptions(bp)
   print "-------------------"
 
-def readLMHOptions(device):
-  bp = RAW_WIRE(device, 115200)
+def readLMHOptions(bp):
   bp.BBmode()
   bp.enter_rawwire()
-  bp.raw_cfg_pins(PinCfg.POWER | PinCfg.CS)
+  bp.raw_cfg_pins(PinCfg.CS)
   if not bp.cfg_raw_wire((RAW_WIRECfg.BIT_ORDER & RAW_WIRE_BIT_ORDER_TYPE.MSB) |
                          (RAW_WIRECfg.WIRES & RAW_WIRE_WIRES_TYPE.TWO) |
                          (RAW_WIRECfg.OUT_TYPE & RAW_WIRE_OUT_TYPE._3V3)):
     raise IOError, "Failed to set pin type correctly"
   bp.set_speed(RAW_WIRESpeed._400KHZ)
   bp.CS_Low()
-  bp.bulk_trans(1, [0x80])
-  datahigh = bp.read_byte()
-  datalow = bp.read_byte()
+  bp.bulk_trans(1, [0xFF])
+  for i in range (0,15):
+    print bp.read_bit()
   bp.CS_High()
   bp.resetBP()
+  
+  print "raw: %u %u" % (datahigh, datalow)
 
   data = (datahigh << 8) | datalow
   if (data & 0x0200) == 0:
@@ -117,6 +120,8 @@ def main():
                       help="numeric (4 bit) value to choose ladder (Table 7)")
   lmhgroup.add_option("-H", "--high-gain", action="store_true", dest="highgain",
                       help="Enable to use the high-gain preamp", default=False)
+  lmhgroup.add_option("-a", "--dis-aux", dest="disaux", action="store_true",
+					  default=False, help="Disable the auxilary output")
   parser.add_option_group(lmhgroup)
 
   (options, args) = parser.parse_args()
@@ -133,7 +138,7 @@ def main():
     setDACValue(options.device, dacaddr[dacmode.index(options.mode)],
                 options.value)
   elif options.mode in lmhmode:
-    setLMHOptions(options.device, options.lp, options.highgain, options.ladder)
+    setLMHOptions(options.device, options.lp, options.highgain, options.ladder, options.disaux)
 
 if __name__ == "__main__":
   main()
